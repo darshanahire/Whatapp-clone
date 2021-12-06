ea087f595c82b6dbc0dd30bf38878d08612640f7
Darshan Ahire <72602965+darshanahire@users.noreply.github.com>
2021-12-06 16:56:36 +0530

    chat page window ui completed

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -16,13 +16,103 @@
         </div>
       </div>
     </div>
-    <div class="mainchatWindow">
-       <!-- <img class="" src="@/assets/chatbg.jpg" alt=""> -->
+    <div class="mainchatWindow scroll-y">
+      <!-- <img class="" src="@/assets/chatbg.jpg" alt=""> -->
+      <div class="dayDiv mt-2">TODAY</div>
+      <div class="topEncrpMsg">
+        <i class="fas fa-lock fa-xs mx-2"></i>
+        Messages are end-to-end encrypted. No one outside of this chat, not even
+        WhatsApp, can read or listen to them. Click to learn more.
+      </div>
+      <div class="container h-100 w-80 py-3">
+        <div class="leftChatParent px-3">
+          <div class="tailInIcon"></div>
+          <div class="leftChat">
+            <p>
+              This is Left chat can you like itThis is Left chat can you like
+              itThis is Left chat can you like itThis is Left chat can you like
+              itThis is Left chat can you like it
+            </p>
+            <div class="chatTime"><p> <span class="mx-1">10:01 am </span></p></div>
+          </div>
+        </div>
+        <div class="rightChatParent px-3">
+          <div class="tailOutIcon"></div>
+          <div class="rightChat">
+            <p>Hello, I am Right chat how Am i ?</p>
+            <div class="chatTime"><p> <span class="mx-1">10:01 am </span> <img src="@/assets/bluetick3.png" alt="" height="10"></p></div>
+          </div>
+        </div>
+        <div class="leftChatParent px-3">
+          <div class="tailInIcon"></div>
+          <div class="leftChat">
+            <p>This is Left chat can you like it</p>
+            <div class="chatTime"><p> <span class="mx-1">10:01 am </span> </p></div>
+          </div>
+        </div>
+        <div class="rightChatParent px-3">
+          <div class="tailOutIcon"></div>
+          <div class="rightChat">
+            <p>Yes I like it you are more attractive that me</p>
+            <div class="chatTime"><p> <span class="mx-1">10:01 am </span> <img src="@/assets/bluetick3.png" alt="" height="10"></p></div>
+          </div>
+        </div>
+        <div class="leftChatParent px-3">
+          <div class="tailInIcon"></div>
+          <div class="leftChat">
+            <p>
+              This is Left chat can you like itThis is Left chat can you like
+              itThis is Left chat can you like itThis is Left chat can you like
+              itThis is Left chat can you like it
+            </p>
+            <div class="chatTime"><p> <span class="mx-1">10:01 am </span></p></div>
+          </div>
+        </div>
+        <div class="rightChatParent px-3">
+          <div class="rightChat">
+            <div class="tailOutIcon"></div>
+            <p>Hello, I am Right chat how Am i ?</p>
+            <div class="chatTime"><p> <span class="mx-1">10:01 am </span> <img src="@/assets/bluetick3.png" alt="" height="10"></p></div>
+          </div>
+        </div>
+        <div class="leftChatParent px-3">
+          <div class="tailInIcon"></div>
+          <div class="leftChat">
+            <p>This is Left chat can you like it</p>
+            <div class="chatTime"><p> <span class="mx-1">10:01 am </span> </p></div>
+          </div>
+        </div>
+        <div class="rightChatParent px-3">
+          <div class="tailOutIcon"></div>
+          <div class="rightChat">
+            <p>Yes I like it you are more attractive that me</p>
+            <div class="chatTime"><p> <span class="mx-1">10:01 am </span> <img src="@/assets/bluetick3.png" alt="" height="10"></p></div>
+          </div>
+        </div>
+
+        <div class="leftChatParent px-3">
+          <div class="tailInIcon"></div>
+          <div class="leftChat">
+            <p>This is Left chat can you like it</p>
+            <div class="chatTime"><p> <span class="mx-1">10:01 am </span> </p></div>
+          </div>
+        </div>
+        <div class="rightChatParent px-3">
+          <div class="tailOutIcon"></div>
+          <div class="rightChat">
+            <p>
+              Hii This is Left chat can you like This is Left chat can you
+              likeThis is Left chat can you like This is Left chat can you like
+            </p>
+            <div class="chatTime"><p> <span class="mx-1">10:01 am </span> <img src="@/assets/bluetick4.png" alt="" height="10"></p></div>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="chatingdatadiv">
       <div class="w-100 d-flex align-items-center justify-content-center">
-        <div class="text-start">
-          <i class="far fa-grin fa-lg text-dark mx-2" ></i>
+        <div class="text-start d-flex">
+          <i class="far fa-grin fa-lg text-dark mx-2"></i>
           <i class="fas fa-paperclip fa-lg text-dark mx-3"></i>
         </div>
         <div class="text-start w-85">
@@ -87,7 +177,104 @@ export default {
   background: #ffff;
   font-size: 15px;
 }
-.w-85{
-    width: 83%;
+.w-85 {
+  width: 83%;
+}
+.w-80 {
+  width: 80%;
+}
+
+.leftChat {
+  height: auto;
+  width: auto;
+  max-width: 50%;
+  padding: 8px 10px 3px;
+  border-radius: 8px;
+  background: white;
+  text-align: start;
+  margin: 6px 0;
+  font-size: 14px;
+  z-index: 5;
+}
+.rightChat {
+  height: auto;
+  width: auto;
+  max-width: 50%;
+  padding: 8px 10px 3px;
+  border-radius: 8px;
+  background: #dcf8c6;
+  text-align: start;
+  margin: 6px 0;
+  font-size: 14px;
+  z-index: 5;
+}
+.leftChatParent {
+  display: flex;
+  justify-content: start;
+  position: relative;
+}
+.rightChatParent {
+  display: flex;
+  justify-content: end;
+  position: relative;
+}
+.tailInIcon {
+  width: 0;
+  height: 0;
+  border-left: 15px solid transparent;
+  border-right: 15px solid transparent;
+  position: absolute;
+  border-top: 16px solid #ffff;
+  z-index: 3;
+  left: 4px;
+  top: 6px;
+  border-radius: 5px;
+}
+.tailOutIcon {
+  width: 0;
+  height: 0;
+  border-left: 15px solid transparent;
+  border-right: 15px solid transparent;
+  position: absolute;
+  border-top: 16px solid #dcf8c6;
+  z-index: 3;
+  right: 4px;
+  top: 6px;
+  border-radius: 5px;
+}
+.chatTime {
+  font-size: 10px;
+  color: rgb(101 101 101);
+  text-align: end;
+  margin:0 2px;
+}
+.topEncrpMsg {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #fdf4c5;
+  color: black !important;
+  width: 78%;
+  font-size: 12px;
+  margin: 20px auto 0;
+  padding: 6px 0;
+  border-radius: 8px;
+}
+.dayDiv {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80px;
+  height: 30px;
+  margin: auto;
+  background: #e1f3fb;
+  border-radius: 6px;
+  font-size: 13px;
+}
+.fa-check-double{
+  color: rgb(96, 200, 235);
+}
+.scroll-y {
+  overflow: scroll;
 }
 </style>
